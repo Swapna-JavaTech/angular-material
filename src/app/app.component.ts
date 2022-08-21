d33a858327581195a308711ea86315b53d70b00e
swapna reniguntla <swapna.hwdigital@gmail.com>
2022-08-21 23:02:31 +0530

    angular sample code added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'onlineshoppingapp';
+  companyName:string = "";
+  contactNumber:number =0;
+  contactEmail:string = "";
+  companyQProfit :number = 0;
+  companyClients:string[] = 
+  ["Hexaware","CTS","Capgemini"];
+  constructor(){
+     this.companyName = "SimpliLearn";
+     this.contactNumber= 932323212;
+     this.contactEmail= "contactus@simplilearn.com";
+     this.companyQProfit = 5677589.6676;
+  }
+
+  addClient(client:string){
+    this.companyClients.push(client);
+  }
+
 }
